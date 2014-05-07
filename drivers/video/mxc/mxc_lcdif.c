7af7bb3a5390ca5c60d324a8b43268b9b97bc71f
Ian Coughlan <ian@embeddedarm.com>
2014-05-07 10:00:59 -0700

    Changes to make the OKAYA LCD work

--- drivers/video/mxc/mxc_lcdif.c
+++ drivers/video/mxc/mxc_lcdif.c
@@ -48,6 +48,12 @@ static struct fb_videomode lcdif_modedb[] = {
 	FB_SYNC_CLK_LAT_FALL,
 	FB_VMODE_NONINTERLACED,
 	0,},
+	
+	{
+	"OKAYA-WVGA", 60, 800, 480, 30066, 50, 70, 0, 0, 50, 50, 
+	FB_SYNC_VERT_HIGH_ACT | FB_SYNC_HOR_HIGH_ACT,
+	FB_VMODE_NONINTERLACED,
+	0,},
 };
 static int lcdif_modedb_sz = ARRAY_SIZE(lcdif_modedb);
 
