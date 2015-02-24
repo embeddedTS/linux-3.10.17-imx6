60be7a75e9c6546202fab27509a0d7f61bcacecb
Mark Featherston <mark@embeddedarm.com>
2015-02-24 09:44:12 -0700

    changed the key dumping to be dev_dbg instead of dev_info.  Otherwise dmesg is flooded with an extra 150 lines each boot.

--- drivers/crypto/caam/sm_test.c
+++ drivers/crypto/caam/sm_test.c
@@ -74,9 +74,9 @@ static void key_display(struct device *dev, u8 *label, u16 size, u8 *key)
 {
 	unsigned i;
 
-	dev_info(dev, label);
+	dev_dbg(dev, label);
 	for (i = 0; i < size; i += 8)
-		dev_info(dev,
+		dev_dbg(dev,
 			 "[%04d] %02x %02x %02x %02x %02x %02x %02x %02x\n",
 			 i, key[i], key[i + 1], key[i + 2], key[i + 3],
 			 key[i + 4], key[i + 5], key[i + 6], key[i + 7]);
@@ -125,7 +125,7 @@ int caam_sm_example_init(struct platform_device *pdev)
 	}
 	unit = 1;
 
-	dev_info(ksdev, "blkkey_ex: %d keystore units available\n", units);
+	dev_dbg(ksdev, "blkkey_ex: %d keystore units available\n", units);
 
 	/* Initialize/Establish Keystore */
 	sm_establish_keystore(ksdev, unit);	/* Initalize store in #1 */
