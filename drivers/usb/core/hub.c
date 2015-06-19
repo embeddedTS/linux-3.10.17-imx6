b19d5f8057bd81015244e978dd350f725527c6d9
Mark Featherston <mark@embeddedarm.com>
2015-06-19 09:08:04 -0700

    Temporarily disabling this warning until a better workaround is found.

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4086,11 +4086,12 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 	else
 		speed = usb_speed_string(udev->speed);
 
-	if (udev->speed != USB_SPEED_SUPER)
+	/* Temporarily disabling until we can find a better solution
+	 * if (udev->speed != USB_SPEED_SUPER)
 		dev_info(&udev->dev,
 				"%s %s USB device number %d using %s\n",
 				(udev->config) ? "reset" : "new", speed,
-				devnum, udev->bus->controller->driver->name);
+				devnum, udev->bus->controller->driver->name);*/
 
 	/* Set up TT records, if needed  */
 	if (hdev->tt) {
