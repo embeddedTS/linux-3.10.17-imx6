6b1ab18fe2995bb99491c743a9c3de41f7626270
Mark Featherston <mark@embeddedarm.com>
2015-12-30 15:38:26 -0700

    Only draw the logo once

--- drivers/video/fbmem.c
+++ drivers/video/fbmem.c
@@ -665,8 +665,7 @@ int fb_show_logo(struct fb_info *info, int rotate)
 {
 	int y;
 
-	y = fb_show_logo_line(info, rotate, fb_logo.logo, 0,
-			      num_online_cpus());
+	y = fb_show_logo_line(info, rotate, fb_logo.logo, 0, 1);
 	y = fb_show_extra_logos(info, y, rotate);
 
 	return y;
