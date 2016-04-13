57fa87aa9f7ae6dc5ee4976e70bfab591a0c1159
Mark Featherston <mark@embeddedarm.com>
2016-04-13 16:08:22 -0700

    Added tsgpio driver

---
+++ include/linux/i2c/tsgpio.h
@@ -0,0 +1,15 @@
+#ifndef _TS_GPIO_H_
+#define _TS_GPIO_H_
+
+#define TSGPIO_OE      0x0001
+#define TSGPIO_OD      0x0002
+#define TSGPIO_ID      0x0004
+
+struct tsgpio_platform_data {
+	u16 ngpio;
+	int base;
+	int twobit;
+	u64 bank;
+};
+
+#endif /* _TS_GPIO_H_ */
