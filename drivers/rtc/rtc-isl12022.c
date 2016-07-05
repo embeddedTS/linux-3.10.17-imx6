eb394dc6a359a5c704ef84bb94bfba3702a3b04f
Mark Featherston <mark@embeddedarm.com>
2016-07-05 13:48:16 -0700

    Set our battery threshholds and enable temp compensation every 10 minutes while on battery

--- drivers/rtc/rtc-isl12022.c
+++ drivers/rtc/rtc-isl12022.c
@@ -20,6 +20,7 @@
 #include <linux/of.h>
 #include <linux/of_device.h>
 
+
 #define DRV_VERSION "0.1"
 
 /* ISL register offsets */
@@ -33,6 +34,9 @@
 
 #define ISL12022_REG_SR		0x07
 #define ISL12022_REG_INT	0x08
+#define ISL12022_REG_VDD	0x09
+#define ISL12022_REG_VBAT	0x0a
+#define ISL12022_REG_BETA	0x0d
 
 /* ISL register bits */
 #define ISL12022_HR_MIL		(1 << 7)	/* military or 24 hour time */
@@ -42,6 +46,14 @@
 
 #define ISL12022_INT_WRTC	(1 << 6)
 
+#define ISL12022_VDD_VB75T_OFFSET	0
+#define ISL12022_VDD_VB75T_MASK		0x7
+#define ISL12022_VDD_VB85T_OFFSET	0
+#define ISL12022_VDD_VB85T_MASK		0x7
+
+#define ISL12022_BETA_TSE	(1 << 7)	/* Enable temp sensor compensation */
+#define ISL12022_BETA_BTSE	(1 << 6)	/* Temp sensor enabled in battery mode */
+#define ISL12022_BETA_BTSR	(1 << 6) 	/* Sample Frequency (1=10min,0=1min) */
 
 static struct i2c_driver isl12022_driver;
 
@@ -115,13 +127,6 @@ static int isl12022_get_datetime(struct i2c_client *client, struct rtc_time *tm)
 	if (ret)
 		return ret;
 
-	if (buf[ISL12022_REG_SR] & (ISL12022_SR_LBAT85 | ISL12022_SR_LBAT75)) {
-		dev_warn(&client->dev,
-			 "voltage dropped below %u%%, "
-			 "date and time is not reliable.\n",
-			 buf[ISL12022_REG_SR] & ISL12022_SR_LBAT85 ? 85 : 75);
-	}
-
 	dev_dbg(&client->dev,
 		"%s: raw data is sec=%02x, min=%02x, hr=%02x, "
 		"mday=%02x, mon=%02x, year=%02x, wday=%02x, "
@@ -165,6 +170,7 @@ static int isl12022_set_datetime(struct i2c_client *client, struct rtc_time *tm)
 	size_t i;
 	int ret;
 	uint8_t buf[ISL12022_REG_DW + 1];
+	uint8_t beta;
 
 	dev_dbg(&client->dev, "%s: secs=%d, mins=%d, hours=%d, "
 		"mday=%d, mon=%d, year=%d, wday=%d\n",
@@ -183,6 +189,32 @@ static int isl12022_set_datetime(struct i2c_client *client, struct rtc_time *tm)
 		if (!(buf[0] & ISL12022_INT_WRTC)) {
 			dev_info(&client->dev,
 				 "init write enable and 24 hour format\n");
+
+			ret = isl12022_read_regs(client, ISL12022_REG_BETA, &beta, 1);
+			if (ret)
+				return ret;
+
+			/* Enable temp reading compensation per 10min  while powered
+			 * by vcc & battery */
+			ret = isl12022_write_reg(client,
+						 ISL12022_REG_BETA,
+						 (beta | ISL12022_BETA_TSE | ISL12022_BETA_BTSE)
+							& ~ISL12022_BETA_BTSR);
+			if (ret)
+				return ret;
+
+			/* Disable VDD trip detection */
+			ret = isl12022_write_reg(client, ISL12022_REG_VDD, 0);
+			if (ret)
+				return ret;
+
+			/* Set VBAT trip levels
+			 * VB85T 0 0 1 (2.295V)
+			 * VB75T 0 0 1 (2.025V)
+			 */
+			ret = isl12022_write_reg(client, ISL12022_REG_VBAT, 0x24);
+			if (ret)
+				return ret;
 
 			/* Set the write enable bit. */
 			ret = isl12022_write_reg(client,
@@ -254,6 +286,8 @@ static int isl12022_probe(struct i2c_client *client,
 			  const struct i2c_device_id *id)
 {
 	struct isl12022 *isl12022;
+	uint8_t buf;
+	int ret;
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C))
 		return -ENODEV;
@@ -266,6 +300,11 @@ static int isl12022_probe(struct i2c_client *client,
 	dev_dbg(&client->dev, "chip found, driver version " DRV_VERSION "\n");
 
 	i2c_set_clientdata(client, isl12022);
+
+	ret = isl12022_read_regs(client, ISL12022_REG_INT, &buf, 1);
+	if (ret){
+		return -ENODEV;
+	}
 
 	isl12022->rtc = devm_rtc_device_register(&client->dev,
 					isl12022_driver.driver.name,
